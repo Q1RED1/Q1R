6070800639f58708cf1b4172767468628c5538be
Q1RED1 <158311350+Q1RED1@users.noreply.github.com>
2024-05-20 16:12:24 +0100

    Update Azure-mod source by Actyrn#7104 (1).lua

--- Azure-mod source by Actyrn#7104 (1).lua
+++ Azure-mod source by Actyrn#7104 (1).lua
@@ -2,7 +2,33 @@
 -- If ur gonna put in vault / showcase, pls don't put the source code directly or loadstring, put the discord
 
 -- DO NOT EDIT BELOW IF YOU DON'T KNOW WHAT YOU'RE DOING!!
+-- For ROBLOX experiences
 
+-- in Server Script object
+
+
+
+local players = game:GetService("Players")
+
+
+
+local accounts = {718907260,0,0} -- Accounts IDs
+
+local kick_message = "You were not in the whitelist. Sorry!" -- Your kick message
+
+
+
+players.PlayerAdded:Connect(function(player)
+
+	if not table.find(accounts,player.UserId) then
+
+		print(player.Name.." attempted to join the game but was not in the whitelist.") -- The script prints a message to indicate to others that an unauthorized player attempted to enter
+
+		PLAYER:Kick(kick_message) -- The players is kicked
+
+	end
+
+end)
 
 repeat wait() until game:IsLoaded()
 
